977330f9351ed3c80a42877b01fb5eff67cc8c57
Sawon <141425167+SawonAnis@users.noreply.github.com>
2025-02-03 00:53:13 +0600

    Add files via upload

---
+++ Codeforces/Boy or Girl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks.Sources;
+
+
+namespace BoyorGirl
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string username=Console.ReadLine(); 
+            HashSet<char> set = new HashSet<char>();
+
+            for (int i = 0;i<username.Length;++i)
+                set.Add(username[i]);   
+                
+if(set.Count%2==0)
+                Console.WriteLine("CHAT WITH HER!");
+          
+            else
+            
+
+                Console.WriteLine("IGNORE HIM!");
+            
+        }
+    }
+
+
+
+}
+
+
+
+
