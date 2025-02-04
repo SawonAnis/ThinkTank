eebf9e5db1ef8e16eba8b3731244d66060e01d05
Sawon <141425167+SawonAnis@users.noreply.github.com>
2025-02-05 02:38:32 +0600

    Add files via upload

---
+++ Codeforces/Watermelon.cs
@@ -0,0 +1,16 @@
+using System;
+
+Console.WriteLine("");
+int w= Convert.ToInt32(Console.ReadLine());
+
+if (w > 2 && w % 2 == 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+
+
+
